767d693d27f3c6b9c839bd4ff32e71d2b2b22f0f
Wojciech Trznadel <wojciech.trznadel@lingarogroup.com>
2021-03-11 21:40:13 +0100

    python

--- Databricks notes/python_notes.py
+++ Databricks notes/python_notes.py
@@ -1,8 +1,29 @@
+import Pyspark
+
 df.show() #- wyswietla dane
+
+#Reading the file
 df = spark.read.csv("csv_file_name.csv", inferSchema = True, header=True)
 
+    #reading multiline json file
+    json_data = spark.read.option('multiline', "true").json("/temp/lesson1/mutliline_file.json")
+
+#Writing to the file
+df.write.mode('append')json('/temp/write_folder/file_name')
+df.write.mode('overwrite')json('/temp/write_folder/file_name')
+
+
+
+#Creating temporary view
 df.createOrReplaceTempView("temp_view_name")
 
+#Creating temporary view with SQL
+%sql
+CREATE TEMPORARY VIEW temp_view_name
+USING csv
+OPTIONS(path "/temp/lesson1/sampletable.csv")
+
+#show dataframe Schema
 df.printSchema()
 
 #--Manipulating Columns--
@@ -32,12 +53,29 @@ dbutils.help()
 dbutils.fs.help()
 dbutils.widgets.help()
 
+#Widgets
+dbutils.widgets.text('variable_name', 'default value', 'label')
+print(dbutils.widgets.get('variable_name'))
+
+
+#creating a file
+dbutils.fs.put('/testfolder/file.txt', 'sample text in the file')
+
+#removing a file
+dbutils.fs.rm("/testfolder/file.txt") #Removes a file
+dbutils.fs.rm('/testfolder/', True) #Removes everything in folder recursivly
+
+#Displaying content of the path
 %fs ls "path/"
 dbutils.fs.ls('path')
+display(dbutils.fs.ls('dbfs://path'))  #displays in tabular view
 
-
+#running a notebook
 dbutils.notebook('/path_to_notebook/notebook.py', 60)    #-> path, timeout
 %run /path_to_notebook/notebook.py $arg1="value1" $arg2="value2" $arg3="value3"
+
+#Exit notebook
+dbutils.notebook.exit('Some parameters value.')
 
 
 #*****READING FROM CSV IN PYSPARK*****
@@ -49,4 +87,6 @@ https://medium.com/understand-the-python/understanding-the-asterisk-of-python-8b
 #*****PASSING XIT VALUES FROM DATABRICKS NOTEBOOK*****
 https://the.agilesql.club/2020/02/passing-status-messages-and-results-back-from-databricks-to-adf/
 
+#*****CUSTOM LOGGING IN DATABRICKS*****
+https://www.youtube.com/watch?v=SnIp6p8px5Q&list=PL50mYnndduIGmqjzJ8SDsa9BZoY7cvoeD&index=11&ab_channel=TechLake
 
